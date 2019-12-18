30c719f0752fb65469f65a77ccbcb06445d6297c
Scullincw <1148135455@qq.com>
2019-12-18 20:19:55 +0800

    提交备份
    修改了“公告”的日期显示

--- page/component/index.js
+++ page/component/index.js
@@ -1,22 +1,15 @@
 var util = require('../../util/util.js');
 Page({
   onShow() {
-    wx.reportAnalytics('enter_home_programmatically', {})
+    
   },
-  onShareAppMessage() {
-    return {
-      title: '移动办公',
-      path: 'page/component/index'
-    }
-  },/*下拉刷新 */
+  
   onLoad: function () {
-    // 调用函数时，传入new Date()参数，返回值是日期和时间
-    var time = util.formatTime(new Date());
-    // 再通过setData更改Page()里面的data，动态更新页面的数据
+    let date = new Date()
     this.setData({
-      time: time
+      date: date.getFullYear() + "年" + date.getMonth() + "月" + date.getDay() + "日"
     })
-    console.log(time)
+    console.log(date)
   },
   data: {
     list: [
@@ -71,15 +64,15 @@ Page({
       }
     ]
   },
-  
+  /*下拉刷新 */
   onPullDownRefresh: function() {
-  wx.showNavigationBarLoading() //在标题栏中显示加载
-  //模拟加载
-  setTimeout(function () {
-    // complete
-    wx.hideNavigationBarLoading() //完成停止加载
-    wx.stopPullDownRefresh() //停止下拉刷新
-  }, 1500);
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
   },
   
   kindToggle(e) {
