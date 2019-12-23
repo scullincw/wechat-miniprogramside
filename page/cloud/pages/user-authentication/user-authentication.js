ed55e881354454af85bbf3a9672a235ed576ebff
Scullincw <1148135455@qq.com>
2019-12-23 21:42:08 +0800

    增加“我的”页面 “我的申请”（展示用）

--- page/cloud/pages/user-authentication/user-authentication.js
+++ page/cloud/pages/user-authentication/user-authentication.js
@@ -1,60 +1,44 @@
-var util = require('../../../../util/util.js');
-Page({
-  onShow() {
 
+const app = getApp()
+
+Page({
+  onLoad() {
+    app.post(
+      'getApplication',
+      {
+        openid: wx.getStorageSync('openid'),
+        skey: wx.getStorageSync('skey'),
+      }
+    )
+      .then(res => {
+        let list = res.data.data;
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i]
+          item.isRead = false   //默认未审批
+          item.open = false //默认不打开详情
+          switch (item.appType) {
+            case 1:
+              item.typeName = '请假'
+              break
+            case 2:
+              item.typeName = '出差'
+              break
+            case 3:
+              item.typeName = '报销'
+              break
+            case 4:
+              item.typeName = '会议室预约'
+              break
+          }
+        }
+        this.setData({
+          applicationList: list
+        })
+      })
   },
   data: {
-    list: [
-      {
-        id: 'view',
-        name: '申请1...',
-        open: false,
-        isRead: false,
-        //pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
-      }, {
-        id: 'content',
-        name: '申请2...',
-        open: false,
-        isRead: false,
-        //pages: ['text', 'icon', 'progress', 'rich-text']
-      }, {
-        id: 'form',
-        name: '申请3...',
-        open: false,
-        isRead: false,
-        //pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea', 'editor']
-      }, {
-        id: 'nav',
-        name: '申请4...',
-        open: false,
-        isRead: false,
-        //pages: ['navigator']
-      }, {
-        id: 'media',
-        name: '申请5...',
-        open: false,
-        isRead: false,
-        //pages: ['image', 'audio', 'video', 'camera']
-      }, {
-        id: 'map',
-        name: '申请6...',
-        open: false,
-        isRead: false,
-        //pages: ['map']
-      }, {
-        id: 'canvas',
-        name: '申请7...',
-        open: false,
-        isRead: false,
-        //pages: ['canvas']
-      }, {
-        id: 'open',
-        name: '申请8...',
-        open: false,
-        isRead: false,
-        //pages: ['ad', 'open-data', 'web-view']
-      }
-    ]
+    applicationList: [],  //申请列表
+
   },
 
   /*下拉刷新 */
@@ -68,20 +52,34 @@ Page({
     }, 1500);
   },
 
+  onSubmit(e) {
+    console.log(e)
+    const id = e.currentTarget.id //组件id
+    let result = e.currentTarget.dataset.result   //审批结果
+    let list = this.data.applicationList  //申请列表
+    for (let i = 0; i < list.length; i++) {
+      let item = list[i]
+      if (item.id == id) {
+        item.isRead = true
+      }
+    }
+
+    this.kindToggle(e)
+  },
+
   kindToggle(e) {
     const id = e.currentTarget.id
-    const list = this.data.list
+    const list = this.data.applicationList
     for (let i = 0, len = list.length; i < len; ++i) {
       if (list[i].id === id) {
         list[i].open = !list[i].open
-        list[i].isRead = true
+        //list[i].isRead = true
       } else {
         list[i].open = false
       }
     }
     this.setData({
-      list
+      applicationList: list
     })
-    wx.reportAnalytics('click_view_programmatically', {})
   }
 })
